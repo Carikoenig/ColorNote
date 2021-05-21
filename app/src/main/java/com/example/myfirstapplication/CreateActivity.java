f08513e5199a125c14adcb558edc8de520803a69
LeoLoewenherz <leonie-leva@t-online.de>
2021-05-21 16:24:43 +0200

    Modus und 1.Taste

--- app/src/main/java/com/example/myfirstapplication/CreateActivity.java
+++ app/src/main/java/com/example/myfirstapplication/CreateActivity.java
@@ -1,14 +1,32 @@
 package com.example.myfirstapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_keyboard_create);
+        setContentView(R.layout.activity_keyboard);
+
+        //Was das C1 auslöst:
+        Button C1_Btn = (Button) findViewById(R.id.C1_Btn);
+        C1_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //bei CreateModus
+                if(MainActivity.modus == 1) {
+
+                }
+                //bei PlayModus
+                else if(MainActivity.modus >= 10){
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.myfirstapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    //0 -> Startbildschirm
+    //1 -> Create
+    //10 aufwärts -> Play (Stücke)
+    public static int modus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchPlay(View v){
         //launch a new activity (specifically in this case the output mode) when clicking on the Play button
-        Intent i = new Intent(this, PlayActivity.class);
-        startActivity(i);
+        Intent gotoKeyboard = new Intent(this, CreateActivity.class);
+        startActivity(gotoKeyboard);
+        modus = 10;
     }
 
     public void launchCreate(View v){
         //launch a new activity (specifically in this case the input mode) when clicking on the Create button
-        Intent i = new Intent(this, CreateActivity.class);
-        startActivity(i);
+        Intent gotoKeyboard = new Intent(this, CreateActivity.class);
+        startActivity(gotoKeyboard);
+        modus = 1;
     }
+
 }
